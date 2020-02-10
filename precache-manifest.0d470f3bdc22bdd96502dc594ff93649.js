78b98ea8df03c83890ff030912a3c898f9f89cc7
bacharuddinadieb <bacharuddinadiebpratama@gmail.com>
2020-02-10 22:58:58 +0700

    Updates

---
+++ precache-manifest.0d470f3bdc22bdd96502dc594ff93649.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ec90a28bfd21c1c25ca5b0a50cbf5e5",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "2d44980652d610c65bad",
+    "url": "/myapp/static/css/2.c35655eb.chunk.css"
+  },
+  {
+    "revision": "db55fed5ca8276ff3bb3",
+    "url": "/myapp/static/css/main.de3080ff.chunk.css"
+  },
+  {
+    "revision": "2d44980652d610c65bad",
+    "url": "/myapp/static/js/2.690ee6b2.chunk.js"
+  },
+  {
+    "revision": "db55fed5ca8276ff3bb3",
+    "url": "/myapp/static/js/main.3b7558aa.chunk.js"
+  },
+  {
+    "revision": "f56cbfd09050d46037d5",
+    "url": "/myapp/static/js/runtime~main.e4f53ea3.js"
+  },
+  {
+    "revision": "6d9a17869a1c92a8c4d83bbc5adcda4d",
+    "url": "/myapp/static/media/add.6d9a1786.svg"
+  },
+  {
+    "revision": "21247cd9cb2dda1e9cd002034f6a3825",
+    "url": "/myapp/static/media/contact.21247cd9.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
